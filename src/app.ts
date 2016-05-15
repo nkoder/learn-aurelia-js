aea59a74e073fd85a8b66390851b5260bd31e402
Pawel Barszcz <pawelbarszcz@gmail.com>
2016-05-16 00:59:54 +0200

    Welcome screen

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,18 @@
+import {RouterConfiguration, Router} from 'aurelia-router';
+
 export class App {
-  message: string = 'Welcome to Aurelia!';
+  router : Router;
+
+  configureRouter(config : RouterConfiguration, router : Router) {
+    config.title = 'Learn Aurelia';
+    config.map([{
+      route: ['', 'welcome'],
+      name: 'welcome',
+      moduleId: './welcome',
+      nav: true,
+      title: 'Welcome'
+    }]);
+
+    this.router = router;
+  }
 }
---
+++ src/welcome.ts
@@ -0,0 +1,14 @@
+export class Welcome {
+
+  heading : string = 'Welcome to Aurelia!';
+  firstName : string = 'John';
+  lastName : string = 'Doe';
+
+  get fullName() : string {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  submit() : void {
+    console.log(`Welcome, ${this.fullName}`);
+  }
+}
