2d9008fb311e02f340e44844915d29130e06eb8a
Pawel Barszcz <pawelbarszcz@gmail.com>
2016-05-16 01:09:57 +0200

    Users screen

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,23 @@
 import {RouterConfiguration, Router} from 'aurelia-router';
 
 export class App {
-  router : Router;
+    router : Router;
 
-  configureRouter(config : RouterConfiguration, router : Router) {
-    config.title = 'Learn Aurelia';
-    config.map([{
-      route: ['', 'welcome'],
-      name: 'welcome',
-      moduleId: './welcome',
-      nav: true,
-      title: 'Welcome'
-    }]);
-
-    this.router = router;
-  }
+    configureRouter(config : RouterConfiguration, router : Router) {
+        config.title = 'Learn Aurelia';
+        config.map([{
+            route: ['', 'welcome'],
+            name: 'welcome',
+            moduleId: './welcome',
+            nav: true,
+            title: 'Welcome'
+        }, {
+            route: 'users',
+            name: 'users',
+            moduleId: './users',
+            nav: true,
+            title: 'Github Users'
+        }]);
+        this.router = router;
+    }
 }
